20bdf8941095a96bc98739a31f7af7532e34696c
Bruno Zell <contact@brunozell.com>
2022-09-04 05:13:35 +0200

    add contract read to poland page

--- packages/react-app/src/components/FundPoland.js
+++ packages/react-app/src/components/FundPoland.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { Button } from '.';
+import { addresses, abis } from "@my-app/contracts";
 
 const About = () => {
+    const { error: contractCallError, value: details } = useCall({
+      contract: new Contract(addresses.legislatoor, abis.legislatoor),
+      method: "details",
+      args: [1], // id of 1 is poland
+    }) ?? { totalContributions: 0 };
+
     return (
         <div>
           <h2>Fund Poland legal fees</h2>
-          <p>Current money pledged: </p>
+          <p>Current money pledged: {details.totalContributions / 18} USDC</p>
           <Button>Connect wallet</Button>
         </div>
     );
